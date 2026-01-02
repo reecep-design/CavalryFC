a69314a5976630e64c998fe883cb5bb69f620514
Paul Reece <reecep@k12teachertools.com>
2026-01-02 13:41:35 -0500

    Fix Express 5 wildcard route error

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
 
     app.use(express.static(distPath));
 
-    app.get('*', (req, res) => {
+    app.get(/(.*)/, (req, res) => {
         res.sendFile(path.join(distPath, 'index.html'));
     });
 }
